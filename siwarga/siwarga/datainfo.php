eda19892bcc89aade84c2c99cca1a310bd85d430
dhimasulfa <dhimasulfa@gmail.com>
2019-04-30 17:21:18 +0700

    tambah file

---
+++ siwarga/siwarga/datainfo.php
@@ -0,0 +1,15 @@
+<?php 
+	include_once "koneksi.php";
+
+	$query = mysqli_query($con, "SELECT * FROM infowarga ORDER BY id ASC");
+	
+	$json = array();	
+	
+	while($row = mysqli_fetch_assoc($query)){
+		$json[] = $row;
+	}
+	
+	echo json_encode($json);
+	
+	mysqli_close($con);
+?>
